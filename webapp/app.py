7789270764002c6483cac0ddb853161644f0282e
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-04-24 16:49:02 +0200

    adding a template to the webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,47 @@
 import os
 
-from flask import Flask, request, render_template
-from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from flask import Flask, request, render_template, flash
 from wtforms.widgets import TextArea
+from flask_bootstrap import Bootstrap
+from flask_appconfig import AppConfig
+from flask_wtf import Form, RecaptchaField
+from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+    BooleanField, SubmitField, IntegerField, FormField, validators, PasswordField
+from wtforms.validators import Required
+
+
+# straight from the wtforms docs:
+class TelephoneForm(Form):
+    country_code = IntegerField('Country Code', [validators.required()])
+    area_code = IntegerField('Area Code/Exchange', [validators.required()])
+    number = TextField('Number')
+
+
+class ExampleForm(Form):
+    field1 = TextField('First Field', description='This is field one.')
+    field2 = TextField('Second Field', description='This is field two.',
+                       validators=[Required()])
+    hidden_field = HiddenField('You cannot see this', description='Nope')
+    recaptcha = RecaptchaField('A sample recaptcha field')
+    radio_field = RadioField('This is a radio field', choices=[
+        ('head_radio', 'Head radio'),
+        ('radio_76fm', "Radio '76 FM"),
+        ('lips_106', 'Lips 106'),
+        ('wctr', 'WCTR'),
+    ])
+    checkbox_field = BooleanField('This is a checkbox',
+                                  description='Checkboxes can be tricky.')
+
+    # subforms
+    mobile_phone = FormField(TelephoneForm)
+
+    # you can change the label as well
+    office_phone = FormField(TelephoneForm, label='Your office phone')
+
+    submit_button = SubmitField('Submit Form')
+
+    def validate_hidden_field(form, field):
+        raise ValidationError('Always wrong')
 
 class RegistrationForm(Form):
     username = TextField('Username',  [validators.Length(min=4, max=25)], default='pire')
@@ -17,6 +56,28 @@ class RegistrationForm(Form):
 
 
 app = Flask(__name__)
+configfile=None
+AppConfig(app, configfile)  # Flask-Appconfig is not necessary, but
+                            # highly recommend =)
+                            # https://github.com/mbr/flask-appconfig
+Bootstrap(app)
+# in a real app, these should be configured through Flask-Appconfig
+app.config['SECRET_KEY'] = 'devkey'
+app.config['RECAPTCHA_PUBLIC_KEY'] = \
+    '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+
+@app.route('/tests', methods=('GET', 'POST'))
+def tests():
+    form = ExampleForm()
+    form.validate_on_submit()  # to get error messages to the browser
+    # flash('critical message', 'critical')
+    # flash('error message', 'error')
+    # flash('warning message', 'warning')
+    # flash('info message', 'info')
+    # flash('debug message', 'debug')
+    # flash('different message', 'different')
+    # flash('uncategorized message')
+    return render_template('index.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -32,7 +93,12 @@ def register():
 @app.route('/')
 def hello():
     provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Hello '+provider+'!'
+    return render_template('base.html', form={'hello':'world'})
+
+@app.route('/landing')
+def landing():
+    return render_template('landing.html', form={'hello':'world'})
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
