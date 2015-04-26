6bbafc41733d1db252ea7dc6ca6e28982deb136c
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-04-26 21:46:25 +0200

    tweaking the text

--- webapp/app.py
+++ webapp/app.py
@@ -66,7 +66,29 @@ def hello():
     provider = str(os.environ.get('PROVIDER', 'world'))
     return render_template('base.html', form={'hello':'world'})
 
-
+import re
+ 
+from jinja2 import evalcontextfilter, Markup, escape
+    
+@app.template_filter()
+@evalcontextfilter
+def linebreaks(eval_ctx, value):
+    """Converts newlines into <p> and <br />s."""
+    value = re.sub(r'\r\n|\r|\n', '\n', value) # normalize newlines
+    paras = re.split('\n{2,}', value)
+    paras = [u'<p>%s</p>' % p.replace('\n', '<br />') for p in paras]
+    paras = u'\n\n'.join(paras)
+    return Markup(paras)
+ 
+@app.template_filter()
+@evalcontextfilter
+def linebreaksbr(eval_ctx, value):
+    """Converts newlines into <p> and <br />s."""
+    value = re.sub(r'\r\n|\r|\n', '\n', value) # normalize newlines
+    paras = re.split('\n{2,}', value)
+    paras = [u'%s' % p.replace('\n', '<br />') for p in paras]
+    paras = u'\n\n'.join(paras)
+    return Markup(paras)
 
 @app.route('/landing', methods=['GET', 'POST'])
 def landing():
