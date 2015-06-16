099badde9584ad1e22057c3d9eb398690430a9a6
IMA <imad@windnordic.dk>
2015-06-16 16:34:27 +0200

    I changed the host address in app.py

--- webapp/app.py
+++ webapp/app.py
@@ -114,4 +114,4 @@ def landing():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
