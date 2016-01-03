575010acae23a351c2b2af7d276379cbebd35931
IMA <imad@windnordic.dk>
2016-01-03 17:21:08 +0100

    Moved the DOI/Keywords searchbox in Emergence from the top right of the navbar to the sidebar

--- app/client/emergence/sidebar.js
+++ app/client/emergence/sidebar.js
@@ -1,3 +1,25 @@
+var doiRegex = '(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)'
+var doiTextPrefix = 'doi\\:'
+findoi = function (opts) {
+  opts = opts || {}
+  return opts.exact ? new RegExp('(?:^' + doiRegex + '$)') :
+                      new RegExp('(?:' + doiRegex + ')', 'g')
+}
+
+Template.search_emergence_side_bar.events({
+  "submit .search": function(event, template){
+    event.preventDefault();
+    var text = event.target.search_text.value;
+    // Test if it's a doi using regex
+    if (findoi({exact:true}).test(text)) {
+      console.log('found it', text);
+      Router.go(`/emergence/doi/${text}`);
+    } else {
+      var keys = text.replace(/ /g, '+');
+      Router.go(`/emergence/search/${keys}`);
+    }
+  }
+});
 
 Template.DOI_Integrations.helpers({
    git: [],
