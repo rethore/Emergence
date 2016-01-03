77ae6f9eb1c8f159895f72954b6755a7f5bd2f49
IMA <imad@windnordic.dk>
2016-01-03 12:35:50 +0100

    - Added "Template.search_box_in_emergence.events" in "navbar.js" so that the search box in the Emergence page works when searching for a DOI or Keywords

--- app/client/emergence/navbar.js
+++ app/client/emergence/navbar.js
@@ -1,7 +1,31 @@
 
+var doiRegex = '(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)'
+var doiTextPrefix = 'doi\\:'
+findoi = function (opts) {
+  opts = opts || {}
+  return opts.exact ? new RegExp('(?:^' + doiRegex + '$)') :
+                      new RegExp('(?:' + doiRegex + ')', 'g')
+}
+
+Template.search_box_in_emergence.events({
+  "submit .search": function(event, template){
+    event.preventDefault();
+    var text = event.target.search_text.value;
+    // Test if it's a doi using regex
+    if (findoi({exact:true}).test(text)) {
+      console.log('found it', text);
+      Router.go(`/emergence/doi/${text}`);
+    } else {
+      var keys = text.replace(/ /g, '+');
+      Router.go(`/emergence/search/${keys}`);
+    }
+  }
+});
+
 UI.registerHelper("there_is", function(value){
   return this.hasOwnProperty(value)
 });
+
 
 Template.DOI_Navbar.helpers({
   publication: function(){
