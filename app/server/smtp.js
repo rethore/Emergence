4df4b33faaeeeee6f1c5f8e6b11c7824ca4b9551
Pierre-Elouan <pe@retho.re>
2016-04-11 11:03:37 +0200

    cleaning up smtp

--- app/server/smtp.js
+++ app/server/smtp.js
@@ -1,3 +1,7 @@
+
 Meteor.startup(function () {
-process.env.MAIL_URL = 'smtp://postmaster%40sandbox64b6251cc38a49a79c9f1c24fdff9267.mailgun.org:45fe623cf9498577a1b7ab268e52aa4e@smtp.mailgun.org:587';
+  var fs = Npm.require('fs');
+  var github_creds = YAML.safeLoad(fs.readFileSync(process.env.PWD + '/.smtp', 'utf8'));
+  console.log('path to smtp creds:',process.env.PWD + '/.smtp');
+  process.env.MAIL_URL = 'smtp://${smtp.url}:587';
 });
